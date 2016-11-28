c13993c72524ef1c72253663fa0e3d5565183f7d
kang01 <904451260@qq.com>
2016-11-28 11:06:18 +0800

    demo

---
+++ YK-app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ykAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ykAppApp
+ */
+angular.module('ykAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
